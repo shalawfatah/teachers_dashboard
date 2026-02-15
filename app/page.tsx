f410ab5de641e4bdb86439a81bc12109cfcadb15
Shalaw Fatah <shalaw.fatah@gmail.com>
2026-02-15 15:21:43 +0300

    moved to proper folders

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,6 @@ export default function Home() {
           </div>
         </nav>
 
-        <DashboardLayout />
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
           <p>
--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -1,9 +1,5 @@
-import { DeployButton } from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
 import { AuthButton } from "@/components/auth-button";
 import { ThemeSwitcher } from "@/components/theme-switcher";
-import { hasEnvVars } from "@/lib/utils";
-import Link from "next/link";
 import { Suspense } from "react";
 
 export default function ProtectedLayout({
