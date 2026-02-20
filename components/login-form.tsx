34c19a7d57e818b9fcd093bccaea9ce05fab0be5
Shalaw Fatah <shalaw.fatah@gmail.com>
2026-02-20 12:42:45 +0300

    added KR to login - signup

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -51,9 +51,9 @@ export function LoginForm({
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader>
-          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardTitle className="text-2xl">چوونە نێو هەژمار</CardTitle>
           <CardDescription>
-            Enter your email below to login to your account
+            ئیمەیلەکەت داخڵ بکە بۆ چوونە نێو هەژمارەکەت
           </CardDescription>
         </CardHeader>
         <CardContent>
@@ -77,7 +77,7 @@ export function LoginForm({
                     href="/auth/forgot-password"
                     className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
                   >
-                    Forgot your password?
+                    وشەی نهێنییت بیرچووە؟
                   </Link>
                 </div>
                 <Input
@@ -90,16 +90,16 @@ export function LoginForm({
               </div>
               {error && <p className="text-sm text-red-500">{error}</p>}
               <Button type="submit" className="w-full" disabled={isLoading}>
-                {isLoading ? "Logging in..." : "Login"}
+                {isLoading ? "چوونە نێو هەژمار..." : "چوونە ناو هەژمار"}
               </Button>
             </div>
             <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?{" "}
+              هەژمارت نیە؟
               <Link
                 href="/auth/sign-up"
                 className="underline underline-offset-4"
               >
-                Sign up
+                دروستکردنی هەژمار
               </Link>
             </div>
           </form>
--- components/sign-up-form.tsx
+++ components/sign-up-form.tsx
@@ -60,14 +60,14 @@ export function SignUpForm({
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader>
-          <CardTitle className="text-2xl">Sign up</CardTitle>
-          <CardDescription>Create a new account</CardDescription>
+          <CardTitle className="text-2xl">دروستکردنی هەژمار</CardTitle>
+          <CardDescription>هەژمارێکی نوێ دروستبکە</CardDescription>
         </CardHeader>
         <CardContent>
           <form onSubmit={handleSignUp}>
             <div className="flex flex-col gap-6">
               <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
+                <Label htmlFor="email">ئیمەیل</Label>
                 <Input
                   id="email"
                   type="email"
@@ -79,7 +79,7 @@ export function SignUpForm({
               </div>
               <div className="grid gap-2">
                 <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
+                  <Label htmlFor="password">وشەی نهێنی</Label>
                 </div>
                 <Input
                   id="password"
@@ -91,7 +91,7 @@ export function SignUpForm({
               </div>
               <div className="grid gap-2">
                 <div className="flex items-center">
-                  <Label htmlFor="repeat-password">Repeat Password</Label>
+                  <Label htmlFor="repeat-password">دووبارەکردنەوەی وشەی نهێنی</Label>
                 </div>
                 <Input
                   id="repeat-password"
@@ -103,13 +103,13 @@ export function SignUpForm({
               </div>
               {error && <p className="text-sm text-red-500">{error}</p>}
               <Button type="submit" className="w-full" disabled={isLoading}>
-                {isLoading ? "Creating an account..." : "Sign up"}
+                {isLoading ? "دروستکردنی هەژماری نوێ..." : "تۆمارکردن"}
               </Button>
             </div>
             <div className="mt-4 text-center text-sm">
-              Already have an account?{" "}
+              هەژمارت دروستکردووە؟
               <Link href="/auth/login" className="underline underline-offset-4">
-                Login
+                چوونە نێو هەژمار
               </Link>
             </div>
           </form>
